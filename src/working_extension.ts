a95066094b4d5d7f116a3e023d63c5e2de66d355
F Lengyel <florian.lengyel@gmail.com>
2023-06-17 15:22:31 -0400

    Added readline

--- src/working_extension.ts
+++ src/working_extension.ts
@@ -3,7 +3,6 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as readline from 'readline';
 
 class MarkdownTreeItem extends vscode.TreeItem {
     constructor(
