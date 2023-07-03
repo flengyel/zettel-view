4e6ae6f93d389527dd15257a772f8153554f7f6c
F Lengyel <florian.lengyel@gmail.com>
2023-07-03 15:48:42 -0400

    Addition of markdownID to class

--- src/AsyncZettelViewTreeItem.ts
+++ src/AsyncZettelViewTreeItem.ts
@@ -1,3 +1,4 @@
+
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,6 +9,7 @@ import { IncomingLinksMap } from './utils/IncomingLinksMap';
 export class AsyncZettelViewTreeItem extends vscode.TreeItem {
     public incomingLinks: Set<string> = new Set<string>();
     private idMatch = false;
+    private markdownID: string;
     
     constructor(
         public readonly pathname: string,
@@ -18,58 +20,59 @@ export class AsyncZettelViewTreeItem extends vscode.TreeItem {
     ) {
         super(basename, collapsibleState);
         this.label = basename;
-        const ID = extractIDFromFilename(basename);
+        this.markdownID = extractIDFromFilename(basename);
 
-        // The rest of your code...
-	
 		// since we cannot make an asynchronous call to a constructor
         // and we want to consume a stream line-by-line, we return
         // an IIAFE: immediately invoked Asynchronous Function Expression
         // See: https://stackoverflow.com/questions/43431550/async-await-class-constructor/50885340#50885340
 
-        return (async (): Promise<AsyncZettelViewTreeItem> =>{
+		return (async (): Promise<AsyncZettelViewTreeItem> =>{
             try {            
                 const fileStream = fs.createReadStream(pathname);
                 const rl = readline.createInterface({
                   input: fileStream,
-                 crlfDelay: Infinity,
+                  crlfDelay: Infinity,
                 });
-                // Read the file line by line
+                
+				// Duplication to remove the error that "this.markdownID is used before it is assigned"
+				this.markdownID = extractIDFromFilename(basename);
+
                 for await (const line of rl) {
                     const h1match = idregex.h1RegExp.exec(line);
                     if (h1match) {
-                        // check if basename == match[1].md
                         if (basename !== `${h1match[1]}.md`) {
                             logger(`ID ${h1match[1]} does not match filename ${basename}`);
-                            // vscode.window.showInformationMessage(`ID ${match[1]} does not match filename ${basename}`);
                         } else {
                             this.idMatch = true;
-							// show the H1 header only when the ID matches the filename
                             this.label = line; 
                         }                          
                     }
-                    // Regex to match markdown links, capture the ID
+                    // all links are of the form [[ID]] or [[ID|TITLE]]
+					// I don't know if Zettlr understands [[ID|TITLE]] 
                     let match;
                     const linkRegex = /\[\[([\w\\.]+)\]\]/g;
                     while ((match = linkRegex.exec(line)) !== null) {
-                        this.incomingLinksMap.addLink(match[1], ID); // Use the extracted ID here
+                        this.incomingLinksMap.addLink(match[1], this.markdownID); 
                     }
-
-                       
                 }
+
                 if (!this.idMatch) {
                     logger(`# ID TITLE not found in: ${basename}`);
                 }
-                // vscode.window.showInformationMessage(`# ID TITLE header not found in: ${basename}`);
-                rl.close();
 
-                // Add the set of incoming links for this file to this object
-                this.incomingLinks = this.incomingLinksMap.getIncomingLinksFor(ID);
+                rl.close();
+                this.incomingLinks = this.incomingLinksMap.getIncomingLinksFor(this.markdownID);
+
                 return this;
             } catch (err) {
                 console.error(err);
                 return this;
             }
         })() as unknown as AsyncZettelViewTreeItem;
+    }
+
+    getID(): string {
+        return this.markdownID;
     }
 }
